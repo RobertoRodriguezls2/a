83563728ad6e12c9ef288f1101d8d3756917436b
RobertoRodriguezls2 <robertrodriguezls26.0@gmail.com>
2022-05-05 20:26:15 -0700

    updated about

--- src/components/Topmenu.js
+++ src/components/Topmenu.js
@@ -22,7 +22,7 @@ class Topmenu extends React.Component{
             <>
     
                 <div class="navbar bg-base-100 bg-carbon/[.25] " >
-                    <div class="navbar-start basis-auto">
+                    <div class="navbar-start basis-auto">   
                         <div class="dropdown">
                             <label tabindex="0" class="btn btn-ghost btn-circle ">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentcolor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
